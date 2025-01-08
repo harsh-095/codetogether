f143360c75d491a06111261b14c7c29f4b308de4
harsh-095 <buddhaharsh5@gmail.com>
2025-01-08 23:57:19 +0530

    Initial commit

---
+++ src/main/java/com/harshapps/codetogether/CodetogetherApplication.java
@@ -0,0 +1,13 @@
+package com.harshapps.codetogether;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodetogetherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodetogetherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/harshapps/codetogether/CodetogetherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.harshapps.codetogether;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodetogetherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
